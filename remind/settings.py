0fae2b1d843d17b5161872009bd57064f161b75a
Aman Sharma <bobbysharma6593@gmail.com>
2022-03-21 16:13:29 +0530

    FORGOT PASSWORD DONE

--- remind/settings.py
+++ remind/settings.py
@@ -132,3 +132,13 @@ STATIC_DIR,
 
 AUTH_USER_MODEL = 'anne.CustomUser'
 AUTHENTICATION_BACKENDS = ['anne.backends.EmailBackend']
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'kudosstatus@gmail.com'
+EMAIL_HOST_PASSWORD = "kudos0212#*"
--- remind/urls.py
+++ remind/urls.py
@@ -39,6 +39,24 @@ urlpatterns = [
     path('cluster/', views.cluster, name = 'cluster'),  
     # path('add-item/', views.addItem, name = 'add-item'), 
     path('add-video/', views.addVideo, name = 'add-video'), 
+    
+     path('reset_password/',
+     auth_views.PasswordResetView.as_view(template_name="anne/password_reset.html"),
+     name="reset_password"),
+
+    path('reset_password_sent/', 
+        auth_views.PasswordResetDoneView.as_view(template_name="anne/password_reset_sent.html"), 
+        name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/',
+     auth_views.PasswordResetConfirmView.as_view(template_name="anne/password_reset_form.html"), 
+     name="password_reset_confirm"),
+
+    path('reset_password_complete/', 
+        auth_views.PasswordResetCompleteView.as_view(template_name="anne/password_reset_done.html"), 
+        name="password_reset_complete"),
+
+
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
