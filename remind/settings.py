21f8093f1fb0d9ee56ebf83549049b4bd25a10e4
Aman Sharma <bobbysharma6593@gmail.com>
2022-03-21 17:03:18 +0530

    2 step verification

--- remind/settings.py
+++ remind/settings.py
@@ -141,4 +141,4 @@ EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'kudosstatus@gmail.com'
-EMAIL_HOST_PASSWORD = "kudos0212#*"
+EMAIL_HOST_PASSWORD = "xbcwnzjbagcwcrje"
