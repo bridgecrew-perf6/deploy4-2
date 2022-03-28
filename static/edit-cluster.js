2b20c014767a5473951fca4207cf46a7ec20254b
Aman Sharma <bobbysharma6593@gmail.com>
2022-03-28 19:16:50 +0530

    search-page and cluster-page enhanced

--- static/edit-cluster.js
+++ static/edit-cluster.js
@@ -24,6 +24,34 @@ function hideDesc() {
   boolc++;
   }
 
+  let sho2 = document.querySelector("#edit-cluster-show-2");
+  sho2.addEventListener("click", show2);
+  function show2(){
+      console.log("show cluster 2");
+      let child = document.querySelector(".edit-cluster-child-2");
+      child.classList.add("edit-cluster-open-child-2");
+      let parent = document.querySelector(".edit-cluster-parent-2");
+      parent.classList.add("edit-cluster-open-2");
+  
+      var elems = document.getElementsByClassName('footer');
+      for (var i=0;i<elems.length;i++){
+          elems[i].style.display = 'none';
+      }
+  }
+  
+  document.querySelector("#hide-editcluster-popup-2").addEventListener("click", hide2);
+  function hide2(){
+      let child = document.querySelector(".edit-cluster-child-2");
+      child.classList.remove("edit-cluster-open-child-2");
+      let parent = document.querySelector(".edit-cluster-parent-2");
+      parent.classList.remove("edit-cluster-open-2");
+  
+      var elems = document.getElementsByClassName('footer');
+      for (var i=0;i<elems.length;i++){
+          elems[i].style.display = 'block';
+      }
+  }
+
 document.querySelector("#hide-editcluster-popup").addEventListener("click", hide);
 let sho = document.querySelector("#edit-cluster-show");
 sho.addEventListener("click", show);
@@ -40,7 +68,7 @@ function show(){
     }
 }
 
-let hid = document.querySelector("#edit-cluster-hide");
+document.querySelector("#hide-editcluster-popup").addEventListener("click", hide);
 function hide(){
     let child = document.querySelector(".edit-cluster-child");
     child.classList.remove("edit-cluster-open-child");
